f09dabc8de892d911a02c2e46381568a5eed7e31
Kawa-1 <kamil-kawka@outlook.com>
2021-06-07 19:21:46 +0200

    final

---
+++ ZADANIA/zad2.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+values_of_letters = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5,
+                         'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4,
+                         'W': 4, 'X': 8, 'Y': 4, 'Z': 10}
+
+
+def place_letters_get_points(board: list_of_lists, letters: ['G', 'I', 'T'], coords: [[7,6][7,7][7,8]]) -> (list_of_lists, \
+        int):
+    # MIEJSCE DO UZUPEŁNIENIA 1)
+
+
+
+
+    return (board, points)
+
+
+class Tablica:
+    # MIEJSCE DO UZUPEŁNIENIA 2)
+
+
+    def board_init(board: list_of_lists) -> list_of_lists:
+        # MIEJSCE DO UZUPEŁNIENIA 2)
+
+
+# W TYM MIEJSCU STWÓRZ OBIEKT Z KLASY TABLICA
+
+# MIEJSCE DO UZUPEŁNIENIA 3)
+
+# MIEJSCE DO UZUPEŁNIENIA 4)
+
+
+
+
+        
+
+
+    
