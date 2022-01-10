d507d2ed91e95a2de60a3193827291e4576eed9b
Kawa-1 <kamil-kawka@outlook.com>
2022-01-10 14:04:09 +0100

    change

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from PyQt6 import QtWidgets
 import sys
 import menu
 from gui_classes.login_window_controller import LoginWindowController
+from management_database import ManagementGeneralLeaderboard
 
 if __name__ == '__main__':
 
     import sys
     app = QtWidgets.QApplication(sys.argv)
     ent = LoginWindowController()
+    ManagementGeneralLeaderboard.delete_empty_games_onstart()
     sys.exit(app.exec())
